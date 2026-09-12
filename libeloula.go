3d814baf9c3600af6b55673f1a7efd2d2739cab3
Dimitrios Kakagiannis <jkakayiannis@gmail.com>
2026-09-12 13:09:11 +0300

    Forgot to register new commands...

--- libeloula.go
+++ libeloula.go
@@ -2,19 +2,26 @@ package libeloula
 
 import (
 	"github.com/df-mc/dragonfly/server/cmd"
-	"github.com/syseditor/libeloula/utils"
 	"github.com/syseditor/libeloula/commands"
+	"github.com/syseditor/libeloula/utils"
 )
 
 var commandMap = make(map[string]cmd.Command, 0)
 
 func Initialize() {
 	utils.LoadOps()
-	RegisterCommands()
+	createCommands()
+	registerAllCommands()
 }
 
-func RegisterCommands() {
-	commandMap["op"] = cmd.New("op", "Gives OP permissions to a specific player", []string{}, commands.Op{})
+func createCommands() {
+	commandMap["op"] = cmd.New("op", "Gives OP permissions to a specific player", []string{"op"}, commands.Op{})
+}
+
+func registerAllCommands() {
+	for _, cm := range commandMap {
+		cmd.Register(cm)
+	}
 }
 
 func GetCommandMap() *map[string]cmd.Command {
