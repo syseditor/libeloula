21bb5f5d16f917e2c47ab0af915463e8c58650dd
Dimitrios Kakagiannis <jkakayiannis@gmail.com>
2026-09-12 12:43:49 +0300

    Command map creation

--- libeloula.go
+++ libeloula.go
@@ -1,9 +1,22 @@
 package libeloula
 
 import (
+	"github.com/df-mc/dragonfly/server/cmd"
 	"github.com/syseditor/libeloula/utils"
+	"github.com/syseditor/libeloula/commands"
 )
+
+var commandMap = make(map[string]cmd.Command, 0)
 
 func Initialize() {
 	utils.LoadOps()
+	RegisterCommands()
+}
+
+func RegisterCommands() {
+	commandMap["op"] = cmd.New("op", "Gives OP permissions to a specific player", []string{}, commands.Op{})
+}
+
+func GetCommandMap() *map[string]cmd.Command {
+	return &commandMap
 }
