b7977a9ba2fab333893dbf192b554bc57f0288eb
Dimitrios Kakagiannis <jkakayiannis@gmail.com>
2026-09-13 15:57:51 +0300

    Clear works, fixing stop

--- source/console/buffer.go
+++ source/console/buffer.go
@@ -62,7 +62,7 @@ loop:
 				bufferContinue <- true
 				continue
 			} else if strings.EqualFold(str, "stop") {
-				terminationSignal <- os.Interrupt
+				terminationSignal <- os.Kill
 				continue
 			}
 
